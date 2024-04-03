2f0eae3dde155b7e1c8f5178f72223a6176b7eca
Duncan McMurtrie <duncan.mcmurtrie@gmail.com>
2024-04-03 15:42:12 +1100

    Working code with error handling. Error handling code is duplicated across each endpoint for now; will investigate how to factor this out.

--- server.js
+++ server.js
@@ -37,37 +37,143 @@ const logger = winston.createLogger({
 const add = (n1, n2) => {
     return n1 + n2;
 }
-
-// Function to validate number parameters for calculator and log errors for invalid numbers
-const validateNumberParams = (n1, n2, endpoint) => {
-    try {
-        const a = parseFloat(n1);
-        const b = parseFloat(n2);
-        if (isNaN(a)) {
-            logger.error("Parameter n1 is not a valid number");
-            throw new Error(`Invalid number ${a} received for parameter n1 at endpoint ${endpoint}`);
-        }
-        if (isNaN(b)) {
-            logger.error("Parameter n2 is not a valid number");
-            throw new Error(`Invalid number ${b} received for parameter n2 at endpoint ${endpoint}`);
-        }
-        return {n1: a, n2: b};
-    } catch (error) {
-        return error;
-    }
+const subtract = (n1, n2) => {
+    return n1 - n2
+}
+const multiply = (n1, n2) => {
+    return n1 * n2;
+}
+const divide = (n1, n2) => {
+    return n1 / n2;
 }
 
-// Endpoint to add two numbers
+// Endpoints
+// Adds n1 and n2 and returns their sum
 // Usage: http://localhost:3000/add?n1=1&n2=5
 app.get("/add", (req, res) => {
-    const n1 = req.query.n1;
-    const n2 = req.query.n2;
     try {
-        params = validateNumberParams(n1, n2, "/add");
-        res.status(200).json({ statuscode: 200, data: add(params.n1, params.n2)});
-        console.log(params);
+        // Try to parse the user's provided parameters as numbers
+        const n1 = req.query.n1;
+        const n2 = req.query.n2;
+        const a = parseFloat(n1);
+        const b = parseFloat(n2);
+
+        // If they're not valid numbers, log and throw an error
+        if (isNaN(a)) {
+            logger.error(`Invalid number '${n1}' received for parameter n2.`);
+            throw new Error(`Invalid number '${n1}' received for parameter n1.`);
+        }
+        if (isNaN(b)) {
+            logger.error(`Invalid number '${n2}' received for parameter n2.`);
+            throw new Error(`Invalid number '${n2}' received for parameter n2.`);
+        }
+
+        // If the parameters are OK (i.e. no error was thrown), subtract n2 from n1 and return the result
+        logger.info(`Numbers ${a} and ${b} received for addition.`);
+        const result = add(a, b);
+        res.status(200).json({ statuscode: 200, data: result });
+
     } catch (error) {
-        console.log("this code has been reached");
+        console.error(error);
+        res.status(500).json({statuscode: 500, msg: error.toString() });
+    }
+    
+});
+
+// Subtracts n2 from n1 and returns this result
+// Usage: http://localhost:3000/subtract?n1=1&n2=5
+app.get("/subtract", (req, res) => {
+    try {
+        // Try to parse the user's provided parameters as numbers
+        const n1 = req.query.n1;
+        const n2 = req.query.n2;
+        const a = parseFloat(n1);
+        const b = parseFloat(n2);
+
+        // If they're not valid numbers, log and throw an error
+        if (isNaN(a)) {
+            logger.error(`Invalid number '${n1}' received for parameter n2.`);
+            throw new Error(`Invalid number '${n1}' received for parameter n1.`);
+        }
+        if (isNaN(b)) {
+            logger.error(`Invalid number '${n2}' received for parameter n2.`);
+            throw new Error(`Invalid number '${n2}' received for parameter n2.`);
+        }
+        
+        // If the parameters are OK (i.e. no error was thrown), subtract n2 from n1 and return the result
+        logger.info(`Numbers ${a} and ${b} received for subtraction.`);
+        const result = subtract(a, b);
+        res.status(200).json({ statuscode: 200, data: result });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({statuscode: 500, msg: error.toString() });
+    }
+    
+});
+
+// Multiplies n1 by n2 and returns this result
+// Usage: http://localhost:3000/mulitply?n1=1&n2=5
+app.get("/multiply", (req, res) => {
+    try {
+        // Try to parse the user's provided parameters as numbers
+        const n1 = req.query.n1;
+        const n2 = req.query.n2;
+        const a = parseFloat(n1);
+        const b = parseFloat(n2);
+
+        // If they're not valid numbers, log and throw an error
+        if (isNaN(a)) {
+            logger.error(`Invalid number '${n1}' received for parameter n2.`);
+            throw new Error(`Invalid number '${n1}' received for parameter n1.`);
+        }
+        if (isNaN(b)) {
+            logger.error(`Invalid number '${n2}' received for parameter n2.`);
+            throw new Error(`Invalid number '${n2}' received for parameter n2.`);
+        }
+        
+        // If the parameters are OK (i.e. no error was thrown), subtract n2 from n1 and return the result
+        logger.info(`Numbers ${a} and ${b} received for multiplication.`);
+        const result = multiply(a, b);
+        res.status(200).json({ statuscode: 200, data: result });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({statuscode: 500, msg: error.toString() });
+    }
+    
+});
+
+// Divides n1 by n2 and returns this result
+// Usage: http://localhost:3000/divide?n1=1&n2=5
+app.get("/divide", (req, res) => {
+    try {
+        // Try to parse the user's provided parameters as numbers
+        const n1 = req.query.n1;
+        const n2 = req.query.n2;
+        const a = parseFloat(n1);
+        const b = parseFloat(n2);
+
+        // If they're not valid numbers, log and throw an error
+        if (isNaN(a)) {
+            logger.error(`Invalid number '${n1}' received for parameter n2.`);
+            throw new Error(`Invalid number '${n1}' received for parameter n1.`);
+        }
+        if (isNaN(b)) {
+            logger.error(`Invalid number '${n2}' received for parameter n2.`);
+            throw new Error(`Invalid number '${n2}' received for parameter n2.`);
+        }
+        if (b == 0) {
+            logger.error("Parameter n2 is zero. Unable to divide by zero");
+            throw new Error("Parameter n2 is zero. Unable to divide by zero");
+        }
+        
+        // If the parameters are OK (i.e. no error was thrown), subtract n2 from n1 and return the result
+        logger.info(`Numbers ${a} and ${b} received for division.`);
+        const result = divide(a, b);
+        res.status(200).json({ statuscode: 200, data: result });
+
+    } catch (error) {
         console.error(error);
         res.status(500).json({statuscode: 500, msg: error.toString() });
     }
